8e445a553fdfbf14c615be7b2917cac63f65a8fe
Harry Huang <harryhuang2652@qq.com>
2022-07-05 12:51:26 +0800

    Update Main-v1.0.py

--- Main-v1.0.py
+++ Main-v1.0.py
@@ -2,8 +2,18 @@
 # Copyright (c) 2022, Harry Huang
 # @ BSD 3-Clause License
 import os, time
-from src import *
+from src.osTool import *
+from src import ResolveAB as AU_Rs
+from src import CombineRGBwithA as AU_Cb
 '''
 ArkUnpacker主程序
 v1.0
 '''
+
+
+if __name__ == '__main__':
+    os.chdir('.')
+    AU_Rs.main(["test"],"temp",dodel=True)
+    os.chdir('.')
+    AU_Cb.main(["temp\\test"],"temp2")
+    exit()
