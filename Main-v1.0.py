ce4282c53bbcc3a4ccd9541c4111b722b2408743
Harry Huang <harryhuang2652@qq.com>
2022-07-06 12:30:18 +0800

    Update Main-v1.0.py

--- Main-v1.0.py
+++ Main-v1.0.py
@@ -2,18 +2,49 @@
 # Copyright (c) 2022, Harry Huang
 # @ BSD 3-Clause License
 import os, time
-from src.osTool import *
-from src import ResolveAB as AU_Rs
+from src.osTool    import *
+from src.colorTool import *
+from src import ResolveAB       as AU_Rs
 from src import CombineRGBwithA as AU_Cb
 '''
 ArkUnpacker主程序
-v1.0
 '''
+AU_ver='v1.0'
+
+
+def prt_homepage():
+    '''
+    #### 打印主页
+    :returns: (none);
+    '''
+    print(color(7)+'欢迎使用ArkUnpacker '+AU_ver)
+
+def prt_subtitle(msg:str):
+    '''
+    #### 打印子标题
+    :param msg: 标题;
+    :returns: (none);
+    '''
+    os.chdir('.')
+    print("")
+    print(color(7,0,1)+'='*10)
+    print(color(7,0,1)+msg)
+    print(color(7,0,1)+'='*10)
+    print(color(7))
+
+def run_quickaccess():
+    '''
+    #### 启动一键执行模式
+    :returns: (none);
+    '''
+    prt_subtitle('步骤1|解包')
+    AU_Rs.main(["test"],"temp",dodel=True)
+    prt_subtitle('步骤2|合并')
+    AU_Cb.main(["temp\\test"],"temp2",dodel=True)
+    exit()
 
 
 if __name__ == '__main__':
-    os.chdir('.')
-    AU_Rs.main(["test"],"temp",dodel=True)
-    os.chdir('.')
-    AU_Cb.main(["temp\\test"],"temp2")
+    prt_homepage()
+    run_quickaccess()
     exit()
