4a5446c72c1c72425b9b2fdab050619ebb6358ee
Harry Huang <harryhuang2652@qq.com>
2024-06-16 19:13:08 +0800

    chore(copyright): increase copyright year to 2024

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2022-2023, Harry Huang
+# Copyright (c) 2022-2024, Harry Huang
 # @ BSD 3-Clause License
 from .Config        import *
 from .GlobalMethods import *
