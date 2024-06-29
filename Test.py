0b774febdd262cb85708fef6a55efd5621de60a3
Harry Huang <harryhuang2652@qq.com>
2024-06-29 16:07:48 +0800

    test(unit): update test script

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2022-2023, Harry Huang
 # @ BSD 3-Clause License
-import os, csv, time, shutil
+import os, sys, csv, time, shutil
 from src import ResolveAB
 from src import CombineRGBwithA
 
@@ -22,9 +22,11 @@ def test_unit_1():
                     doaud=True,
                     dospine=True
                     )
+        if __count_files('test/upk') != 1269:
+            raise AssertionError("Upk files count error")
         return time.time() - t
-    except BaseException:
-        return -1
+    except BaseException as arg:
+        return arg
 
 def test_unit_2():
     try:
@@ -33,20 +35,31 @@ def test_unit_2():
                             'test/cmb',
                             dodel=False
                             )
+        if __count_files('test/cmb') != 112:
+            raise AssertionError("Cmb files count error")
         return time.time() - t
-    except BaseException:
-        return -1
+    except BaseException as arg:
+        return arg
 
 if __name__ == '__main__':
-    print("Preparing...")
-    shutil.rmtree('test/upk', ignore_errors=True)
-    shutil.rmtree('test/cmb', ignore_errors=True)
-    print("Testing...")
-    result = [["Unit1", test_unit_1()],
-            ["Unit2", test_unit_2()]
-            ]
-    with open('test/result.csv', 'w', newline='', encoding='UTF-8') as f:
-        csv.writer(f).writerows(result)
-    print("Test done!")
-    print("Time consumption results: (-1 for failed)")
-    print(result)
+    for i in range(int(sys.argv[1]) if len(sys.argv) > 1 else 1):
+        print(f"#{i} Preparing...")
+        shutil.rmtree('test/upk', ignore_errors=True)
+        shutil.rmtree('test/cmb', ignore_errors=True)
+        print(f"#{i} Testing...")
+        result = [["Unit1", test_unit_1()],
+                ["Unit2", test_unit_2()]
+                ]
+        for n, t in result:
+            if type(t) in (float, int):
+                print(f"[OKAY] {n} used {round(t, 3)}s")
+            elif type(t) == AssertionError:
+                print(f"[FAIL] {n} assertion failed, {t.args}")
+            elif type(t) == BaseException:
+                print(f"[FAIL] {n} raised {t}")
+            else:
+                raise TypeError("Unknown result")
+        with open('test/result.csv', 'a', newline='', encoding='UTF-8') as f:
+            for n, t in result:
+                csv.writer(f).writerow((n, t if type(t) in (float, int) else -1))
+        print(f"#{i} Test done!")
