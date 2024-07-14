0445c7c48522311436d65f01aba5fa6edb130d49
Harry Huang <harryhuang2652@qq.com>
2024-07-14 20:30:09 +0800

    refactor: optimize collecting models

--- src/CollectModels.py
+++ src/CollectModels.py
@@ -1,134 +1,121 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2022-2024, Harry Huang
 # @ BSD 3-Clause License
-import os.path, re
+import os.path, re, shutil
 from .utils import*
 
 
-def get_oper_common_name(str):
-    def get_oper_id(str):
-        rst = re.findall(r'char_[0-9]+_', str.lower())
-        if len(rst): #char_000_
-            return rst[0][5:-1]
-        return ""
-    def get_oper_name(str):
-        rst = re.findall(r'char_[0-9]+_.+\.', str.lower())
-        if len(rst): #char_000_xxx(_xx).
-            rst = re.findall(r'_.+\.', rst[0]) 
-            if len(rst): #_000_xxx(_xx).
-                if len(re.findall(r'_.+$', rst[0])):
-                    rst = re.findall(r'_.+$', rst[0][1:])
-                    if len(rst):
-                        return rst[0][1:-2]
-                elif len(re.findall(r'_.+\.', rst[0])):
-                    rst = re.findall(r'_.+\.', rst[0][1:])
-                    if len(rst):
-                        return rst[0][1:-1]
-        return ""
-    id, name = get_oper_id(str), get_oper_name(str)
-    return "" if len(id) * len(name) == 0 else f"{id}_{name}"
-
-def get_enemy_common_name(str):
-    def get_enemy_id(str):
-        rst = re.findall(r'enemy_[0-9]+_', str.lower())
-        if len(rst): #enemy_000_
-            return rst[0][6:-1]
-        return ""
-    def get_enemy_name(str):
-        rst = re.findall(r'enemy_[0-9]+_.+\.', str.lower())
-        if len(rst): #enemy_000_xxx(_xx).
-            rst = re.findall(r'_.+\.', rst[0]) 
-            if len(rst): #_000_xxx(_xx).
-                if len(re.findall(r'_.+$', rst[0])):
-                    rst = re.findall(r'_.+$', rst[0][1:])
-                    if len(rst):
-                        return rst[0][1:-2]
-                elif len(re.findall(r'_.+\.', rst[0])):
-                    rst = re.findall(r'_.+\.', rst[0][1:])
-                    if len(rst):
-                        return rst[0][1:-1]
-        return ""
-    id, name = get_enemy_id(str), get_enemy_name(str)
-    return "" if len(id) * len(name) == 0 else f"{id}_{name}"
-
-def get_dyn_illust_common_name(str):
-    return "" if len(get_oper_common_name(str)) == 0 else 'dyn_illust_' + get_oper_common_name(str)
-
-def contains_file(dir, filename):
-    if os.path.isdir(dir):
-        for j in get_filelist(dir):
-            if os.path.basename(j) == filename and os.path.isfile(j):
-                return True
-    return False
+def collect_models(upkdir:str, destdir:str, dodel:bool, on_finished:staticmethod, on_collected:staticmethod):
+    """
+    
+    """
+    error_occurred = False
+    for model_type_dir in get_dirlist(upkdir, max_depth=1):
+        model_type:str = os.path.basename(model_type_dir) # Sub dir of one model type
+        for model_dir in get_dirlist(model_type_dir, max_depth=1):
+            model:str = os.path.basename(model_dir) # Sub dir of one determined model
+            if not model.islower():
+                # To solve model typo caused by Arknights side
+                model = model.lower()
+                Logger.warn(f"CollectModels: \"{model_dir}\" may has a typo name")
+            try:
+                newname = None
+                if model_type.startswith('Building') and re.match(r'(build_)?char_', model):
+                    newname = re.match(r'(build_)?char_(\d+_[0-9a-zA-Z]+(_[0-9a-zA-Z#]+)?)', model).group(2)
+                elif model_type.startswith('Battle') and re.match(r'enemy_', model):
+                    newname = re.match(r'enemy_(\d+_[0-9a-zA-Z]+(_\d+)?)', model).group(1)
+                elif model_type.startswith('DynIllust') and re.match(r'dyn_illust_char_', model):
+                    newname = "dyn_illust_" + re.match(r'dyn_illust_char_(\d+_[0-9a-zA-Z]+(_[0-9a-zA-Z#]+)?)', model).group(1)
+                if newname:
+                    # Move to 
+                    dest = os.path.join(destdir, newname)
+                    Logger.debug(f"CollectModels: \"{model_dir}\" -> \"{dest}\"")
+                    shutil.copytree(model_dir, dest, dirs_exist_ok=True)
+                    rmdir(model_dir)
+                    if on_collected:
+                        on_collected()
+                else:
+                    # Not matched any rules
+                    pass 
+            except Exception as arg:
+                error_occurred = True
+                Logger.error(f"CollectModels: Error occurred while handling \"{model_dir}\": Exception{type(arg)} {arg}")
+    if dodel and not error_occurred:
+        rmdir(upkdir)
+    if on_finished:
+        on_finished()
 
 ########## Main-主程序 ##########
-def main(srcdirs:"list[str]", destdirs:"list[str]", dodel:bool=False):
+def main(srcdirs:"list[str]", destdirs:"list[str]"):
     """Collects the Spine models from the source directories to the destination directories accordingly.
+    The structure of the source directory is shown below.
+
+    ```
+    ├─source_dir
+    │  ├─unpacked_dir
+    │  │  ├─model_type_dir
+    │  │  │  ├─model_dir
+    │  │  │  │  ├─files
+    ```
+
     :param srcdirs: Source directories list;
     :param destdirs: Destination directories list;
-    :param dodel: Whether to delete the existed destination directories first, `False` for default;
     :rtype: None;
     """
     print("\n正在解析目录...", s=1)
     Logger.info("CollectModels: Reading directories...")
     if len(srcdirs) != len(destdirs):
+        Logger.error("CollectModels: Arguments error")
         print("参数错误", c=3)
         return
     
-    flist = [] #[(subsub-srcdir, sub-srcdir, destdir),...]
-    for src, dest in zip(srcdirs, destdirs):
-        print("\t正在读取目录 "+src)
-        for dir1 in get_dirlist(src, max_depth=1):
-            for dir2 in get_dirlist(dir1, max_depth=1):
-                flist.append((dir2, dir1, dest))
+    flist = [] # [(upkdir, destdir), ...]
+    for srcdir, destdir in zip(srcdirs, destdirs):
+        print(f"\t正在读取目录 {srcdir}")
+        for upkdir in get_dirlist(srcdir, max_depth=1):
+            flist.append((upkdir, destdir))
     
-    cont_f = 0 #已分拣模型计数
-    cont_p = 0 #进度百分比计数
-    if dodel:
-        print("\n正在清理...", s=1)
-        for i in destdirs:
-            print("\t正在清理目录 "+i)
-            rmdir(i) #慎用，会预先删除目的地目录的所有内容
+    TC = ThreadCtrl(PerformanceLevel.get_thread_limit(Config.get('performance_level')))
+    collected = Counter()
+    UI = UICtrl(0.5)
     TR = TimeRecorder()
     TR.update_dest(1, len(flist))
+    on_finished = lambda: TR.done_once(1)
+    on_collected = lambda: collected.update()
 
-    clear()
-    for dir2, dir1, dest in flist:
-        #递归处理各个目录(i是元组(sub-srcdir, destdir))
-        print(f'正在分拣模型...', y=1)
-        print(f'|{progress_bar(cont_p, 25)}| {color(2, 0, 1)}{round(cont_p*100, 1)}%', y=2)
-        print(f'当前目录：\t{dir2}', y=3)
-        print(f'累计分拣：\t{cont_f}', y=4)
-        print(f'剩余时间：\t{round(TR.get_remaining_time() / 60, 1)}min', y=5)
+    UI.reset()
+    UI.loop_start()
+    for upkdir, destdir in flist:
+        #(i stands for a source dir's path)
+        TR_p = TR.get_progress()
+        TR_r = TR.get_remaining_time()
+        UI.request([
+            f'正在分拣模型...',
+            f'|{progress_bar(TR_p, 25)}| {color(2, 0, 1)}{round(TR_p*100, 1)}%',
+            f'当前搜索：\t{os.path.basename(upkdir)}',
+            f'累计分拣：\t{collected.now()}',
+            f'剩余时间：\t{f"{round(TR_r / 60, 1)}min" if TR_r > 0 else "计算中"}',
+        ])
         ###
-        try:
-            dir1_base = os.path.basename(dir1)
-            dir2_base = os.path.basename(dir2)
-            models = get_dirlist(dir2, max_depth=1)
-            if (dir2_base in ['Building']) and 'char_' in dir1_base:
-                for m in models:
-                    newname = get_oper_common_name(os.path.basename(m)+'.')
-                    mvfile(m, os.path.join(dest, newname))
-                    cont_f += 1
-            elif 'enemy_' in dir1_base:
-                for m in models:
-                    newname = get_enemy_common_name(os.path.basename(m)+'.')
-                    mvfile(m, os.path.join(dest, newname))
-                    cont_f += 1
-            elif (dir2_base in ['DynIllust']) and 'char_' in dir1_base:
-                for m in models:
-                    m_base = os.path.basename(m)
-                    if '_start' not in m_base.lower() and 'dyn_illust_' in m_base.lower():
-                        newname = get_dyn_illust_common_name(m_base+'.')
-                        mvfile(m, os.path.join(dest, newname))
-                        cont_f += 1
-        except BaseException as arg:
-            Logger.error(f'CollectModels: Error occurred while handling "{dir2}": Exception{type(arg)} {arg}')
-        TR.done_once(1)
-        cont_p = TR.get_progress()
+        TC.run_subthread(collect_models, (upkdir, destdir, True, on_finished, on_collected), \
+            name=f"CmThread:{id(upkdir)}")
 
-    clear()
+    UI.reset()
+    UI.loop_stop()
+    while TC.count_subthread() or not SafeSaver.get_instance().completed() or TR.get_progress() < 1:
+        TR_p = TR.get_progress()
+        TR_r = TR.get_remaining_time()
+        UI.request([
+            f'正在分拣模型...',
+            f'|{progress_bar(TR_p, 25)}| {color(2, 0, 1)}{round(TR_p*100, 1)}%',
+            f'累计分拣：\t{collected.now()}',
+            f'剩余时间：\t{f"{round(TR_r / 60, 1)}min" if TR_r > 0 else "计算中"}',
+        ])
+        UI.refresh(post_delay=0.2)
+
+    UI.loop_stop()
+    UI.reset()
     print(f'\n分拣模型结束!', s=1)
-    print(f'  累计分拣 {cont_f} 套模型')
+    print(f'  累计分拣 {collected.now()} 套模型')
     print(f'  此项用时 {round(TR.get_consumed_time())} 秒')
     time.sleep(2)
