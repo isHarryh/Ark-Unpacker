796e003bde3e9cf8aac6b6c93046b1ed8cdb8b05
Harry Huang <harryhuang2652@qq.com>
2024-07-17 21:40:01 +0800

    test(unit): update test script

--- Test.py
+++ Test.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2022-2023, Harry Huang
 # @ BSD 3-Clause License
-import os, sys, csv, time, shutil
+import os, sys, json, shutil
 from src import ResolveAB
 from src import CombineRGBwithA
+from src.utils.AnalyUtils import TestRT
+from src.utils.GlobalMethods import *
 
 def __count_files(path):
     count = 0
@@ -11,55 +13,37 @@ def __count_files(path):
         count += len(files)
     return count
 
-def test_unit_1():
-    try:
-        t = time.time()
-        ResolveAB.main('test/res',
-                    'test/upk',
-                    dodel=False,
-                    doimg=True,
-                    dotxt=True,
-                    doaud=True,
-                    dospine=True
-                    )
-        if __count_files('test/upk') != 1307:
-            raise AssertionError("Upk files count error")
-        return time.time() - t
-    except BaseException as arg:
-        return arg
-
-def test_unit_2():
-    try:
-        t = time.time()
-        CombineRGBwithA.main('test/upk',
-                            'test/cmb',
-                            dodel=False
-                            )
-        if __count_files('test/cmb') != 113:
-            raise AssertionError("Cmb files count error")
-        return time.time() - t
-    except BaseException as arg:
-        return arg
-
 if __name__ == '__main__':
     for i in range(int(sys.argv[1]) if len(sys.argv) > 1 else 1):
-        print(f"#{i} Preparing...")
-        shutil.rmtree('test/upk', ignore_errors=True)
-        shutil.rmtree('test/cmb', ignore_errors=True)
-        print(f"#{i} Testing...")
-        result = [["Unit1", test_unit_1()],
-                ["Unit2", test_unit_2()]
-                ]
-        for n, t in result:
-            if type(t) in (float, int):
-                print(f"[OKAY] {n} used {round(t, 3)}s")
-            elif type(t) == AssertionError:
-                print(f"[FAIL] {n} assertion failed, {t.args}")
-            elif type(t) == BaseException:
-                print(f"[FAIL] {n} raised {t}")
-            else:
-                raise TypeError("Unknown result")
-        with open('test/result.csv', 'a', newline='', encoding='UTF-8') as f:
-            for n, t in result:
-                csv.writer(f).writerow((n, t if type(t) in (float, int) else -1))
-        print(f"#{i} Test done!")
+        try:
+            print(f"[#{i}] Preparing...", c=0, bg=6)
+            shutil.rmtree('test/upk', ignore_errors=True)
+            shutil.rmtree('test/cmb', ignore_errors=True)
+
+            print(f"[#{i}] Testing...", c=0, bg=6)
+            with TestRT('unit_1'):
+                ResolveAB.main('test/res',
+                            'test/upk',
+                            dodel=False,
+                            doimg=True,
+                            dotxt=True,
+                            doaud=True,
+                            dospine=False
+                            )
+            with TestRT('unit_2'):
+                CombineRGBwithA.main('test/upk',
+                                    'test/cmb',
+                                    dodel=False
+                                    )
+            
+            print(f"[#{i}] Analysing...", c=0, bg=6)
+            if __count_files('test/upk') != 1262:
+                raise AssertionError("Upk files count error")
+            if __count_files('test/cmb') != 139:
+                raise AssertionError("Cmb files count error")
+            
+            print(f"[#{i}] Test success!", c=0, bg=2)
+        except BaseException as arg:
+            print(f"[#{i}] Test failed because an error occurred!", c=7, bg=1)
+            print(stacktrace(), c=3)
+    json.dump(TestRT.get_avg_time_all(), open('test/rt.json', 'w', encoding='UTF-8'), indent=4)
