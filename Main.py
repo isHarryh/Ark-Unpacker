88399f0a947a70315fc903b4f943e34e803db144
Harry Huang <harryhuang2652@qq.com>
2024-08-01 10:28:28 +0800

    chore: bump to v3.1

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from src import CombineRGBwithA as AU_Cb
 from src import CollectModels   as AU_Cm
 from src import ModelsDataDist  as AU_Mdd
 
-AU_ver = 'v3.0'
+AU_ver = 'v3.1'
 AU_i18n = 'zh-CN'
 
 
