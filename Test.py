5419a135fa3a8bc1741eb09e12f54409c132ae29
Harry Huang <harryhuang2652@qq.com>
2025-03-09 14:46:41 +0800

    test(unit): add res

--- Test.py
+++ Test.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
                                 )
 
             print(f"[#{i}] Analysing...", c=0, bg=6)
-            if __count_files(DIR_UPK) != 1300:
+            if __count_files(DIR_UPK) != 1374:
                 raise AssertionError("Unpacked files count mismatch")
-            if __count_files(DIR_CMB) != 140:
+            if __count_files(DIR_CMB) != 153:
                 raise AssertionError("Combined images count mismatch")
             if __count_files(DIR_DTA) != 2:
                 raise AssertionError("Decoded textassets count mismatch")
