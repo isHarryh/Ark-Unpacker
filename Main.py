c572561111f3bc02230244aeb894a1e5c73bc495
Harry Huang <harryhuang2652@qq.com>
2025-05-18 12:31:19 +0800

    chore: bump to v4.1

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@
 # @ BSD 3-Clause License
 from typing import Callable, Optional
 
-import argparse
 import os
 import os.path as osp
 import re
@@ -24,7 +23,7 @@ from src import CollectVoice as AU_Cv
 from src import ModelsDataDist as AU_Mdd
 from src import VoiceDataDist as AU_Vdd
 
-ARKUNPACKER_VERSION = "v4.0"
+ARKUNPACKER_VERSION = "v4.1"
 ARKUNPACKER_LOCAL = "zh-CN"
 
 
