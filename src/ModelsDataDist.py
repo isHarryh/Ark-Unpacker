688b774dbf45dc21a2869cda6779902bfbbbae3b
Harry Huang <harryhuang2652@qq.com>
2025-10-12 11:49:06 +0800

    chore: bump to v4.3

--- src/ModelsDataDist.py
+++ src/ModelsDataDist.py
@@ -53,7 +53,7 @@ class ModelsDist:
         self.data = {
             "storageDirectory": ModelsDist.MODELS_DIR,
             "sortTags": ModelsDist.SORT_TAGS_L10N,
-            "gameDataVersionDescription": f"Producer: ArkUnpacker 3\nDate: {datetime.now().date()}\n",
+            "gameDataVersionDescription": f"Producer: ArkUnpacker 4\nDate: {datetime.now().date()}\n",
             "gameDataServerRegion": ModelsDist.SERVER_REGION,
             "data": {},
             "arkPetsCompatibility": ModelsDist.ARK_PETS_COMPATIBILITY,
