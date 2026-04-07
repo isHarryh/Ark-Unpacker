fa5cdb67480b1180d5daa811975431b224821ca9
Harry Huang <harryhuang2652@qq.com>
2026-04-07 23:36:29 +0800

    chore: bump to v5.0

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ from src import ModelsDataDist as AU_Mdd
 from src import VoiceDataDist as AU_Vdd
 from src import ResolveUSM as AU_Usm
 
-ARKUNPACKER_VERSION = "v4.5"
+ARKUNPACKER_VERSION = "v5.0"
 ARKUNPACKER_LOCAL = "zh-CN"
 CLI = RichCLI.get_instance()
 
